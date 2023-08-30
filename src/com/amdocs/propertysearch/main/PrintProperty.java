1483a1470937880317dd919e87e4c4719274f75f
c1te <hkerketta310@gmail.com>
2023-08-30 11:02:23 +0530

    updated Formatting And Scanner Function

--- src/com/amdocs/propertysearch/main/PrintProperty.java
+++ src/com/amdocs/propertysearch/main/PrintProperty.java
@@ -10,11 +10,12 @@ public class PrintProperty {
 		
 		Iterator<Property> i = res.iterator();
 	
-		
+		System.out.printf("%5s %5s %5s %5s %7s %10s %14s %6s %12s","propertyId","Room No","Area(sqft)","Floor","City","State","Cost(Lakh)","Owner","Contact No");
+		System.out.println();
 		while(i.hasNext()) {
 			Property curr = i.next() ;
 			
-			System.out.format("%5s %5s %10s %5s %10s %10s %5s %5s %10s",
+			System.out.format("%5s %8s %11s %6s %10s %12s %8s %8s %14s",
 					curr.getpropertyId(),curr.getNoOfRooms(),curr.getArea(),curr.getFloorNo(),
 					curr.getCity(),curr.getState(),curr.getCost(),curr.getOwnerName(),curr.getOwnerContactNo());
 			System.out.println();
